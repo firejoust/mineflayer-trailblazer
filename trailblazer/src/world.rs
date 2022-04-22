2805ed822f631258576699c413da0bd40a35e631
Firejoust <42525540+firejoust@users.noreply.github.com>
2022-04-22 12:51:41 +1000

    world: add type structures

---
+++ trailblazer/src/world.rs
@@ -0,0 +1,15 @@
+struct ChunkSection {
+    palette: Vec<u32>,
+    data: [u32; 512],
+    max_bits: u32,
+    bits_per_value: u32,
+    values_per_long: u32,
+    value_mask: u32,
+    capacity: u32,
+}
+
+struct ChunkColumn {
+    chunk_x: i32,
+    chunk_y: i32,
+    sections: Vec<ChunkSection>
+}
